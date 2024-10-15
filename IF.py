92ffa9ddb467045756229a950eb3b5e012201688
FANGxPC <md.sadiqueimam2024@vitstudent.ac.in>
2024-10-15 16:41:28 +0530

    1st code addaasasfmn

---
+++ IF.py
@@ -0,0 +1,32 @@
+
+lists=['sukuna','jogo','madara','obito','zatch']
+
+for list in lists:
+    if list=='sukuna' or list=='obito':
+        print(f"{list.title()} can defeat Sataro Gojo")
+    else:
+        print(f"{list.title()} can't defeat Sataro Gojo \n")
+
+
+    
+lists=['sukuna','jogo','madara','obito','zatch']
+win='zatch'
+
+if win not in lists:
+    print(f"{win.title()} can't defeat Naruto")
+else:
+    print(f"{win.title()} can never defeat Naruto")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
