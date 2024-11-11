6780f99528a3951d0f3fcbdf8adcceb690453726
FANGxPC <md.sadiqueimam2024@vitstudent.ac.in>
2024-11-12 01:18:37 +0530

    nothing special

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class custom_sett:
         self.screen_height=800
         self.bg_color=(255,255,255)
         self.ship_speed=1.5
-        self.bullet_speed=1.0
+        self.bullet_speed=3.0
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=(60,60,60)
